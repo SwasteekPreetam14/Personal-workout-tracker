9069767e3368c1071da270e9c53921597bf49782
Swasteek Preetam <54988748+SwasteekPreetam14@users.noreply.github.com>
2022-07-02 09:13:09 +0530

    Update exercise.model.js

--- backend/models/exercise.model.js
+++ backend/models/exercise.model.js
@@ -6,7 +6,7 @@ const exerciseSchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   duration: { type: Number, required: true },
-  date: { type: Date, required: true },
+  day: { type: String, required: true },
 }, {
   timestamps: true,
 });
